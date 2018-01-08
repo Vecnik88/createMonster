865ac82c0d292eda2dd55a11ad3010719bf32c0f
Vecnik <Ve_cni_k@inbox.ru>
2018-01-09 04:15:32 +0700

    refactoring

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Simple game "Create Monster"
 
-![alt text](screenshots/screenshot.png)
+![alt text](screenshots/game1.png)
+![alt text](screenshots/game2.png)
+![alt text](screenshots/game3.png)
--- scripts/monster.js
+++ scripts/monster.js
@@ -1,57 +1,82 @@
 $(document).ready(function() {
-	lighting_one(4000);
-	lighting_two(5000);
-	lighting_three(7000);
-
-	var headClix = 0, eyeClix = 0, noseClix = 0, mouthClix = 0;
-	$("#head").click(function() {
-		console.log("head");
-		if (headClix < 9) {
-			$(this).animate({left: "-=367"}, 500);
-			headClix += 1;
-		} else {
+	goLightning();
+	window.onblur = stopLightning;
+	window.onfocus = goLightning;
+	var int1, int2, int3;
+	var w = 367;
+	var m = 10;
+	$("#btnRandom").click(randomize);
+	$("#btnReset").click(reset);
+	function getRandom(number) {
+		return Math.floor(Math.random() * number);
+	}
+	function randomize() {
+		$(".face").each(function(index) {
+			var target_position = getRandom(m);
+			var current_position = clicks[index];
+			clicks[index] = target_position;
+			if (target_position > current_position) {
+				var move_to = (target_position - current_position) * w;
+				$(this).animate({left: "-=" + move_to +"px"}, 500);
+			} else if (target_position < current_position) {
+				var move_to = (current_position - target_position) * w;
+				$(this).animate({left: "+=" + move_to +"px"}, 500);
+			}
+		});
+	}
+	function reset() {
+		$(".face").each(function(index) {
+			clicks[index] = 0;
 			$(this).animate({left: "0px"}, 500);
-			headClix = 0;
+		});
+	}
+	function goLightning() {
+		int1 = setInterval(function() {
+			lighting_one();
+		}, 4000);
+		int2 = setInterval(function() {
+			lighting_two();
+		}, 5000);
+		int3 = setInterval(function() {
+			lighting_three();
+		}, 7000);
+	}
+	function stopLightning() {
+		window.clearInterval(int1);
+		window.clearInterval(int2);
+		window.clearInterval(int3);
+	}
+	var clicks = [0, 0, 0, 0];
+	function moveMe(obj, index) {
+		if (clicks[index] < 9) {
+			$(obj).animate({left: "-=367px"}, 500);
+			++clicks[index];
+		} else {
+			$(obj).animate({left: "0px"}, 500);
+			clicks[index] = 0;
 		}
+
+	}
+	$("#head").click(function() {
+		moveMe(this, 0);
 	});
 	$("#eyes").click(function() {
-		if (eyeClix < 9) {
-			$(this).animate({left: "-=367"}, 500);
-			eyeClix += 1;
-		} else {
-			$(this).animate({left: "0px"}, 500);
-			eyeClix = 0;
-		}
+		moveMe(this, 1);
 	});
 	$("#nose").click(function() {
-		if (noseClix < 9) {
-			$(this).animate({left: "-=367"}, 500);
-			noseClix += 1;
-		} else {
-			$(this).animate({left: "0px"}, 500);
-			noseClix = 0;
-		}
+		moveMe(this, 2);
 	});
 	$("#mouth").click(function() {
-		if (mouthClix < 9) {
-			$(this).animate({left: "-=367"}, 500);
-			mouthClix += 1;
-		} else {
-			$(this).animate({left: "0px"}, 500);
-			mouthClix = 0;
-		}
+		moveMe(this, 3);
 	});
 });
 // функции сверкания молний
-function lighting_one(t) {
+function lighting_one() {
 	$("#lighting1").fadeIn(250).fadeOut(250);
-	setTimeout("lighting_one()", t);
 }
-function lighting_two(t) {
+function lighting_two() {
 	$("#lighting2").fadeIn(250).fadeOut(250);
-	setTimeout("lighting_two()", t);
 }
-function lighting_three(t) {
+function lighting_three() {
 	$("#lighting3").fadeIn(250).fadeOut(250);
-	setTimeout("lighting_three()", t);
 }
